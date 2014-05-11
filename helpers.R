60f5b6a8a39c52e5fc9fa1c2275b4b728ba43e74
Jean-Francois Cantin <jfcantin@gmail.com>
2014-05-10 17:19:45 -0700

    Cleaned up file processing and filtering.

--- helpers.R
+++ helpers.R
@@ -1,7 +1,8 @@
-getElectricPowerDataset <- function(){
+getElectricPowerDatasetWithFixedRange <- function(){
     if(!file.exists("data")){
         dir.create("data")
     }
+    
     zipfile <- "data//electricPower.zip"
     if(!file.exists(zipfile)){
         fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
@@ -12,18 +13,20 @@ getElectricPowerDataset <- function(){
     
     houseHoldPowerFileName <- "household_power_consumption.txt"
     
-    data <- read.table(unz(zipfile, houseHoldPowerFileName), header=TRUE,sep=";", 
-                       colClasses="character", nrows=2076000)
+    columnClasses <- c(rep("factor", 2), rep("numeric", 7))
+    data <- read.table(unz(zipfile, houseHoldPowerFileName), 
+                       header=TRUE,
+                       sep=";", 
+                       colClasses=columnClasses,
+                       nrows=2076000, na.strings="?")
     
-    startDate <- as.Date("2007-02-01", format="%Y-%m-%d")
-    endDate <- as.Date("2007-02-02", format="%Y-%m-%d")
-    data <- data[data$Date >= startDate & data$Date <= endDate,]    
-    
-    datetime <- paste(data[,1], data[,2],sep=" ")
-    datetime <- strptime(datetime, format="%d/%m/%Y %H:%M:%S")
-    data[,1] <- as.Date(data[,1], format="%d/%m/%Y")
-    data[,3:9] <- lapply(data[3:9], as.numeric)
-    data <- cbind(data, datetime)
-    #a<-read.table("data//household_power_consumption.txt", header=FALSE,sep=";", nrows=1000)
+    startDate <- "1/2/2007"
+    endDate <- "2/2/2007"
+    data <- data[data$Date == startDate | data$Date == endDate,]    
+
+    datetime <- paste(data$Date, data$Time, sep=" ")
+    data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+    data$Time <- strptime(datetime, format="%d/%m/%Y %H:%M:%S")
+
     return(data)
 }
